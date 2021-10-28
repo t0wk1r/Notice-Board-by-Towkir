6e8837dda7a0d90fea768fd856cdc56ca28ea383
MD. ABU SAYED <64466834+t0wk1r@users.noreply.github.com>
2021-10-28 14:45:19 +0600

    Update widget-form.php

--- notice-board-by-towkir/inc/widget-form.php
+++ notice-board-by-towkir/inc/widget-form.php
@@ -1,12 +1,12 @@
 <p>
-	<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
-	<input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" 
-		name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" />
+	<label for="<?php echo esc_attr ($this->get_field_id('title')); ?>">Title:</label>
+	<input type="text" class="widefat" id="<?php echo esc_attr ($this->get_field_id('title')); ?>" 
+		name="<?php echo esc_attr ($this->get_field_name('title')); ?>" value="<?php echo esc_attr ($title); ?>" />
 </p>
 
 <p>
-	<label for="<?php echo $this->get_field_id('count'); ?>">No. of notices to display: </label>
-	<input type="number" size="2" id="<?php echo $this->get_field_id('count'); ?>" 
+	<label for="<?php echo esc_attr ($this->get_field_id('count')); ?>">No. of notices to display: </label>
+	<input type="number" size="2" id="<?php echo esc_attr ( $this->get_field_id('count')); ?>" 
 		name="<?php echo esc_attr ($this->get_field_name('count')); ?>" value="<?php echo $count; ?>" />
 </p>
 
@@ -19,8 +19,8 @@
 </p>
 
 <p>
-	<label for="<?php echo $this->get_field_id('dir'); ?>">Direction: </label>
-	<select id="<?php echo $this->get_field_id('dir'); ?>" name="<?php echo $this->get_field_name('dir') ?>">
+	<label for="<?php echo esc_attr  ($this->get_field_id('dir')); ?>">Direction: </label>
+	<select id="<?php echo esc_attr  ($this->get_field_id('dir')); ?>" name="<?php echo esc_attr ($this->get_field_name('dir')) ?>">
 		<option value="up" <?php selected( $dir, 'up' );?> >Upwards</option>
 		<option value="down" <?php selected( $dir, 'down' );?> >Downwards</option>
 	</select>
